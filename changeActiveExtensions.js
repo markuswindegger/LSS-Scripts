4a62044b6f41da8da9258ed1085e7e766d404019
floflo3299 <59307199+floflo3299@users.noreply.github.com>
2023-07-07 10:33:06 +0200

    Update and rename changeExtensionsStatus to changeActiveExtensions.js

--- changeActiveExtensions.js
+++ changeActiveExtensions.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Change active Extensions
+// @name         de-/activate Extensions
 // @namespace    http://tampermonkey.net/
 // @version      1
 // @description  Change the status of expensions
