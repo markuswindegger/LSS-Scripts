79f25d0fcfad100fdf163bf073a4235065d28883
floflo3299 <59307199+floflo3299@users.noreply.github.com>
2023-12-29 21:58:23 +0100

    Create PerformActionOnMultipleBuildingsInSchool.js

---
+++ PerformActionOnMultipleBuildingsInSchool.js
@@ -0,0 +1,114 @@
+// ==UserScript==
+// @name         PerformActionOnMultipleBuildingsInSchool
+// @namespace    http://tampermonkey.net/
+// @version      v0.1
+// @description  try to take over the world!
+// @author       Silberfighter
+// @match        https://www.leitstellenspiel.de/buildings/1633111
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=leitstellenspiel.de
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var forwardClick = true
+
+    var alleWachen = document.getElementsByClassName("panel-heading personal-select-heading")
+
+    //console.log(alleWachen)
+
+    for (let ele of alleWachen){
+        ele.addEventListener('click', function(event){
+            var targetElement = event.target || event.srcElement;
+            addListenerToDropDown(targetElement);
+        }, false);
+    }
+
+    function addListenerToDropDown(ElementClickedOn){
+        waitUntilLoaded(ElementClickedOn.nextElementSibling)
+
+    }
+
+    function waitUntilLoaded(elemToCheck){
+        if(elemToCheck.childElementCount == 1){
+            //console.log("relaoded")
+            setTimeout(function(){
+                waitUntilLoaded(elemToCheck);
+            }, 100);
+        }else{
+            //console.log("laoded")
+
+            let allDropDownElements = elemToCheck.getElementsByTagName("li")
+
+
+            for (let ele of allDropDownElements){
+                ele.addEventListener('click', function(event){
+                    if(forwardClick){
+                        var targetElement = event.target || event.srcElement;
+                        listElementClicked(targetElement);
+                    }
+                }, false);
+            }
+        }
+    }
+
+
+    function listElementClicked(ElementClickedOn){
+        forwardClick = false
+
+        //console.log(ElementClickedOn)
+        //console.log(ElementClickedOn.onclick.toString())
+
+        let withOutSkill = ElementClickedOn.className == "schooling_select_available_without_education"
+        let courseName = '"ohneAusbildung"'
+
+        if (!withOutSkill){
+            courseName = '"' + ElementClickedOn.onclick.toString().split('"')[1] + '"'
+        }
+
+        console.log(withOutSkill)
+        console.log(courseName)
+
+
+        let allLIClasses = document.getElementsByTagName("li")
+
+        for (let ele of allLIClasses){
+            let relevantChoild = ele.firstElementChild
+            if (relevantChoild != null && (!withOutSkill && relevantChoild.className == "schooling_select_available_with_education" || withOutSkill && relevantChoild.className == "schooling_select_available_without_education")){
+                //console.log(relevantChoild.onclick.toString().indexOf(courseName))
+
+                if(relevantChoild.parentElement.parentElement.parentElement.parentElement.className == "panel-body"){
+                    if(!withOutSkill && relevantChoild.onclick.toString().indexOf(courseName) >= 0 || withOutSkill && relevantChoild.className == "schooling_select_available_without_education"){
+                        relevantChoild.click()
+                    }
+                }
+            }
+        }
+
+        forwardClick = true
+    }
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
